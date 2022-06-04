ea8e424e62f5ed4dae0a039a514ba1ecdd32a561
Pedro Henrique <pedrophaab@hotmail.com>
2022-06-04 14:27:23 -0300

    Cleaned backingStore.c

--- Libs/backingStore/backingStore.c
+++ Libs/backingStore/backingStore.c
@@ -3,18 +3,11 @@
 
 void findPageInBackingStore(int page, signed char returnArray[256]) {
 
-        FILE *file;
-        signed char valuesFromBackingStore[256];
-        file = fopen("BACKING_STORE.bin", "rb");
         int pageStartInBackingStore = page * 256;
-        
+
+        FILE *file;
+        file = fopen("BACKING_STORE.bin", "rb");
         fseek(file, pageStartInBackingStore, SEEK_SET);
-
-        fread(valuesFromBackingStore, sizeof(signed char), 256, file);
-        
+        fread(returnArray, sizeof(signed char), 256, file);
         fclose(file);
-
-        for (int i = 0; i < 256; i++) {
-                returnArray[i] = valuesFromBackingStore[i];
-        }
 }
